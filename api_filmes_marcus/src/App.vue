395323bbbef7e0a3fa8e6551a3df6558f4835f97
Astatsu <vinijao123690@gmail.com>
2022-10-04 09:16:09 -0300

    vala_videos

---
+++ api_filmes_marcus/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import ObjetoView from "./views/ObjetoView.vue";
+export default {
+  components: { ObjetoView },
+};
+</script>
+
+<template>
+  <ObjetoView />
+</template>
+
+<style></style>
---
+++ api_filmes_marcus/src/views/ObjetoView.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <i><h1>Vala Video</h1></i>
+    <i><h1>Séries</h1></i>
+    <i><h1>Filmes</h1></i>
+    <i><h1>Gêneros</h1></i>
+    <i><h1>Sobre Nós</h1></i>
+  </header>
+</template>
+
+<style>
+header {
+  display: flex;
+}
+
+h1 {
+  font-family: "Open Sans", sans-serif;
+  color: rgb(95, 95, 93);
+  padding: 40px;
+  padding-top: 0px;
+}
+</style>
